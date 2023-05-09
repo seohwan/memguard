20ddb443a4c2670f7b31d372b78a6649e92ca9ce
seohwan <seohwan91@gmail.com>
2023-05-09 16:36:35 +0900

    revert budget

--- memguard/memguard.c
+++ memguard/memguard.c
@@ -503,7 +503,7 @@ static void period_timer_callback_slave(struct core_info *cinfo)
 	/* new budget assignment from user */
 	if (cinfo->limit > 0) {
 		/* limit mode */
-		cinfo->budget = (cinfo->limit * 4);
+		cinfo->budget = (cinfo->limit);
 	} 
 
 	/* budget can't be zero? */
